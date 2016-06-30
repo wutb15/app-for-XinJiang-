c1057aca75fa88b86cf05722a3a344cd185f822e
Siyu Wang <wang_sy1997@163.com>
2016-06-30 19:01:45 +0800

    modified:   xinjiangsite/app/Familycondition.php 	modified:   xinjiangsite/database/seeds/DatabaseSeeder.php 	new file:   xinjiangsite/resources/views/test.blade.php

--- xinjiangsite/app/Familycondition.php
+++ xinjiangsite/app/Familycondition.php
@@ -11,12 +11,35 @@ class Familycondition extends Model
     protected $fillable=[
         'familyname','familylocation'
     ];
-    protected $mFamilyIncome;
+    protected $mFamilyIncomePerMonth;
     protected $mFamilyLevel;
-    private  function calFamilyIncome(){}
-    private  function calFamilyLevel(){}
-    
-    public  function getFamilyIncome(){}
-    public  function getFamilyLevel(){}
+    private function members(){
+        return $this->hasMany('App\Individualcondition');
+    }
+
+    private  function calFamilyIncome(){
+        $members= Familycondition::members();
+        foreach( $members as $member)
+            $this->mFamilyIncomePerMonth=$this->mFamilyIncomePerMonth+$member->Income;
+    }
+    private  function calFamilyLevel(){
+        if ($this->mFamilyIncomePerMonth <= 1000)
+            $this->mFamilyLevel = "非常贫困";
+        else if ($this->mFamilyIncomePerMonth <= 3000)
+            $this->mFamilyLevel = "较为贫困";
+        else if ($this->mFamilyIncomePerMonth <= 4000)
+            $this->mFamilyLevel = "一般";
+        else  $this->mFamilyLevel = "富裕";
+    }
+    public  function getFamilyIncome(){
+        return $this->mFamilyIncomePerMonth;
+    }
+    public  function getFamilyLevel()
+    {
+      return $this->mFamilyLevel;
+    }
+
+    }
+
     //
-}
+
