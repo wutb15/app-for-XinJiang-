d70b90f1bb5b3e52fa7fc9500a5d102cad830083
jkq2015 <jkq15@mails.tsinghua.edu.cn>
2016-07-03 21:15:30 +0800

    change FamilyController.php

--- xinjiangsite/app/Http/Controllers/FamilyController.php
+++ xinjiangsite/app/Http/Controllers/FamilyController.php
@@ -2,32 +2,66 @@
 
 namespace App\Http\Controllers;
 
+use App\Familycondition;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
 
 class FamilyController extends Controller
 {
+    protected $fields = [
+        'family_id' => 0,
+        'family_name' => '',
+        'familylocation' => '',
+    ];
 
     public function __construct()
     {
-        //$this->middleware('auth');
+        $this->middleware('auth');
     }
 
-    public function create(Request $request){}
-    
-    
-    public function delete($id){}
-    
-    
-    public function edit(Request $request){}
-    
-    
-    public function show($id){}//一个家庭信息，外加成员简略信息如身份证号
-    public function  search()
-    {
-        return view('family.search');
+    public function create(Request $request){
+        $family = new Familycondition;
+        foreach (array_keys($this->fields) as $field){
+            $family->$field = $request->get($field);
+        }
+        $family->save();
     }
+    
+    
+    public function delete($id){
+        $result=Familycondition::find($id);
+        if (!$result) return redirect('/failure')->withErrors("找不到信息!");
+        else{
+            $result->delete();
+            return redirect('/family/show/{id}');
+        }   //delete,return
+    }
+    
+    
+    public function edit(Request $request){
+        $fam_id =$request->input('id');
+        $result=Familycondition::find($fam_id);
+        if (!$result) return redirect('/failure')->withErrors("找不到信息!");
+        else{
+            foreach (array_keys($this->fields) as $field){
+                $result->$field = $request->input($field);
+            }
+            $result->save();
+            return redirect('/family/show/{id}');
+        }
+    }
+    
+    
+    public function show($id){
+        return view('family.show')->with(Familycondition::with('members'))->find($id);
+    }//一个家庭信息，外加成员简略信息如身份证号
 
-    //
+
+    public function search(Request $request){
+        $fam_id =$request->input('id');
+        $result=Familycondition::find($fam_id);
+        if (!$result)  return redirect('/failure')->withErrors("找不到信息!");
+        else return redirect('/family/show/{id}');        //待定
+    }//
 }
