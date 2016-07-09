1d146cb6b31fa63a58be70d6bc4d1dc8dc29ecd0
tasteofapple <tasteofapple@vip.qq.com>
2016-07-09 16:28:34 +0800

    modify height of pic

--- xinjiangsite/resources/views/home.blade.php
+++ xinjiangsite/resources/views/home.blade.php
@@ -13,19 +13,19 @@
     <!-- Wrapper for slides -->
     <div class="carousel-inner" role="listbox">
         <div class="item active">
-            <img src="img/1.jpg" alt="1 slide">
+            <img src="img/1.jpg" alt="1 slide" height="300px">
             <div class="carousel-caption">
 
             </div>
         </div>
         <div class="item">
-            <img src="img/2.jpg" alt="2 slide">
+            <img src="img/2.jpg" alt="2 slide" height="300px>
             <div class="carousel-caption">
                 
             </div>
         </div>
         <div class="item">
-            <img src="img/3.jpg" alt="3 slide">
+            <img src="img/3.jpg" alt="3 slide" height="300px>
             <div class="carousel-caption">
 
             </div>
