c310c3b07645a70dd2fce5dd45714c8687a0b2d3
tasteofapple <tasteofapple@vip.qq.com>
2016-07-09 11:52:32 +0800

    add explanations

--- xinjiangsite/resources/views/home.blade.php
+++ xinjiangsite/resources/views/home.blade.php
@@ -60,16 +60,19 @@
         <div class="col-md-4">
             <img class="img-circle" src="img/4.jpg" alt="1">
             <h2>搜索家庭信息</h2>
+            <p>在这里可以快速搜索到登记过的家户信息。</p>
             <p><a class="btn btn-default" href="#" role="button">点我进入</a></p>
         </div>
         <div class="col-md-4">
             <img class="img-circle" src="img/5.jpg" alt="2">
             <h2>新建家户</h2>
+            <p>在这里可以新建家户，为流动人员登记信息。</p>
             <p><a class="btn btn-default" href="#" role="button">点我进入</a></p>
         </div>
         <div class="col-md-4">
             <img class="img-circle" src="img/6.jpg" alt="3">
             <h2>搜索个人信息</h2>
+            <p>在这里可以根据身份证号快速搜索到登记过的个人信息。</p>
             <p><a class="btn btn-default" href="#" role="button">点我进入</a></p>
         </div>
     </div>
