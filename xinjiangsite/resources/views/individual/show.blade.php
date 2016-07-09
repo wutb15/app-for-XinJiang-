580c8e89a7c4e3f642b286aa77a7502eb786cc77
ly-liu15 <ly-liu15@mails.tsinghua.edu.cn>
2016-07-09 17:08:59 +0800

    change show

--- xinjiangsite/resources/views/individual/show.blade.php
+++ xinjiangsite/resources/views/individual/show.blade.php
@@ -2,23 +2,32 @@
 
 @section('content')
     <div class="container">
-        <form role="form" method="post" action="{{url('individual/update')}}">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+        <form role="form" method="post" class="form-horizontal" action="{{url('individual/update')}}">
             {!!csrf_field()!!}
-                <div class="col-md-6 col-offset-3">
-                    <label for="Idcardid">身份证号 </label>
-                    <input type="text" id="Idcardid" name="Idcardid" value="{{$individual->Idcardid}}" class="form-control" readonly>
 
+                <div class="form-group">
+                    <label for="Idcardid">身份证号 </label>
+                    <div class="col-md-6">
+                    <input type="text" id="Idcardid" name="Idcardid" value="{{$individual->Idcardid}}" class="form-control" readonly>
+                    </div>
                 </div>
-                <div class="col-md-6 col-offset-3">
+                <div class="form-group">
                     <label for="name">姓名</label>
+                    <div class="col-md-6">
                     <input type="text" id="name" name="name" value="{{$individual->name}}" class="form-control" readonly>
+                    </div>
                 </div>
-                <div class="col-md-6 col-offset-3">
+                <div class="form-group">
                     <label for="income">年收入</label>
+                    <div class="col-md-6">
                     <input type="text" id="income" name="income" value="{{$individual->income}}" class="form-control" readonly>
+                    </div>
                 </div>
-                <div class="col-md-6 col-offset-3">
+                 <div class="form-group">
                     <label for="sex">性别</label>
+                    <div class="col-md-6">
                     <select class="form-control" name="sex" id="sex" disabled>
                         @if($individual->sex==0)
                             <option value="0" selected>男</option>
@@ -29,36 +38,42 @@
                         @endif
 
                     </select>
+                    </div>
                 </div>
-                <div class="col-md-6">
+                <div class="form-group">
                     <label for="date">生日</label>
-                </div>
+
                     <div class="col-md-6 date">
                         <div class="input-group input-append date" id="datePicker">
                             <input type="text" class="form-control" name="birthday" id="date" value="{{$individual->birthday}}" readonly>
                             <span class="input-group-addon add-on"><span class="glyphicon glyphicon-calendar"></span></span>
                         </div>
                     </div>
+                    <input type="hidden" name="family_id" value="{{$individual->family_id}}">
+                    </div>
 
-                <input type="hidden" name="family_id" value="{{$individual->family_id}}">
-
-
-                <div class="col-md-6 col-offset-3">
+            <div class="form-group">
+                    <div class="col-md-6">
                     <input type="submit" id="submit" name="submit" value="提交更改" class="btn-primary form-control">
+                     </div>
                 </div>
-                <div class="col-md-6 col-offset-3">
+                    <div class="form-group">
+                        <div class="col-md-6">
                     <input type="button" id="edit" name="edit" value="编辑文档" class="btn-primary form-control" onclick="enableEdit()">
-                </div>
+                        </div>
+                    </div>
         </form>
-             <div class="col-md-6 col-offset-3">
+                <div class="form-group">
 
-                 <form role="form" method="post" action="{{route('individual.delete',['id'=>$individual->Idcardid])}}">
-
+                     <form role="form" method="post" action="{{route('individual.delete',['id'=>$individual->Idcardid])}}">
+                     <div class="col-md-6">
                      <input type="submit" id="delete" name="delete" value="删除此文档" class="btn-warning form-control">
+                         </div>
+                     </form>
+                </div>
 
-                 </form>
              </div>
-
+        </div>
     </div>
    <script>
 
