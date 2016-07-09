979415a2cb26f26a9c7ded21a9a7e389bd784e84
tasteofapple <tasteofapple@vip.qq.com>
2016-07-09 11:14:27 +0800

    add pictures

--- xinjiangsite/resources/views/home.blade.php
+++ xinjiangsite/resources/views/home.blade.php
@@ -14,4 +14,45 @@
         </div>
     </div>
 </div>
+<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+        <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+        <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+        <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner" role="listbox">
+        <div class="item active">
+            <img src="img/1.jpg" alt="1 slide">
+            <div class="carousel-caption">
+
+            </div>
+        </div>
+        <div class="item">
+            <img src="img/2.jpg" alt="2 slide">
+            <div class="carousel-caption">
+                
+            </div>
+        </div>
+        <div class="item">
+            <img src="img/3.jpg" alt="3 slide">
+            <div class="carousel-caption">
+
+            </div>
+        </div>
+    </div>
+
+    <!-- Controls -->
+    <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+        <span class="sr-only">上一页</span>
+    </a>
+    <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+        <span class="sr-only">下一页</span>
+    </a>
+</div>
+
 @endsection
